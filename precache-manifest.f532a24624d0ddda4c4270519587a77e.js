19f16f2205df529acb22ba57f21c111633ffb85f
ballyees <kittinan_prompao@elearning.cmu.ac.th>
2020-03-24 14:51:32 +0700

    Updates

---
+++ precache-manifest.f532a24624d0ddda4c4270519587a77e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d110ccecf34a6df723b0ed1e6e517df0",
+    "url": "/lab8-deploy/index.html"
+  },
+  {
+    "revision": "6fd203f184ef37709c22",
+    "url": "/lab8-deploy/static/css/2.62ebf70e.chunk.css"
+  },
+  {
+    "revision": "fd859bac509d87a1e2da",
+    "url": "/lab8-deploy/static/css/main.0b974771.chunk.css"
+  },
+  {
+    "revision": "6fd203f184ef37709c22",
+    "url": "/lab8-deploy/static/js/2.1db18afa.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/lab8-deploy/static/js/2.1db18afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd859bac509d87a1e2da",
+    "url": "/lab8-deploy/static/js/main.8cc04b98.chunk.js"
+  },
+  {
+    "revision": "31d5c02eb12b4f4070d0",
+    "url": "/lab8-deploy/static/js/runtime-main.584fb22e.js"
+  },
+  {
+    "revision": "cb83a55ca9b912aa11d18153b5d58758",
+    "url": "/lab8-deploy/static/media/dev.cb83a55c.jfif"
+  }
+]);
